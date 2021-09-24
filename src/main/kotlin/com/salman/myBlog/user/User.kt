673368f91919eff63fac012ecc3d085784be65de
Salman A <salman.a@hatio.in>
2021-09-24 12:20:10 +0530

    Map user class to table in database

--- src/main/kotlin/com/salman/myBlog/user/User.kt
+++ src/main/kotlin/com/salman/myBlog/user/User.kt
@@ -1,9 +1,24 @@
 package com.salman.myBlog.user
 
 import java.time.LocalDate
+import javax.persistence.*
 
-data class User(var id: Long,
+@Entity
+@Table(name = "users")
+data class User(
+                @Id
+                @SequenceGenerator(
+                    name = "user_sequence",
+                    sequenceName = "user_sequence",
+                    allocationSize = 1
+                )
+                @GeneratedValue(
+                    strategy = GenerationType.SEQUENCE,
+                    generator = "user_sequence"
+                )
+                var id: Long,
                 var name: String,
                 var email: String,
                 var dob: LocalDate,
-                var age: Int)
+                var age: Int
+                )
