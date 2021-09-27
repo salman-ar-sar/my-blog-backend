3d9fb1851d7170989ec49829fc2cce665f64635e
Salman A <salman.a@hatio.in>
2021-09-27 11:01:49 +0530

    Implement delete user function

--- src/main/kotlin/com/salman/myBlog/user/UserService.kt
+++ src/main/kotlin/com/salman/myBlog/user/UserService.kt
@@ -15,4 +15,12 @@ class UserService(private val userRepository: UserRepository) {
         }
         userRepository.save(user)
     }
+
+    fun deleteUser(userId: Long) {
+        val exists: Boolean = userRepository.existsById(userId)
+        if (!exists) {
+            throw IllegalStateException("User with ID:$userId doesn't exits")
+        }
+        userRepository.deleteById(userId)
+    }
 }
