e648418c50b560de55a5c979a2fccc108333a0da
Salman A <salman.a@hatio.in>
2021-09-27 10:15:58 +0530

    Remove age from user creation

--- src/main/kotlin/com/salman/myBlog/user/UserConfig.kt
+++ src/main/kotlin/com/salman/myBlog/user/UserConfig.kt
@@ -11,9 +11,9 @@ class UserConfig {
 
     @Bean
     fun commandLineRunner(userRepository: UserRepository)= CommandLineRunner {
-        val alan = User(1L,"Alan","alan@gmail.com", LocalDate.of(2004, MAY, 10), 21)
-        val alex = User(2L, "Alex","alex@gmail.com", LocalDate.of(1974, MAY, 10), 41)
-        val anil = User(3L,"Anil","anil@gmail.com",LocalDate.of(2000, MAY, 10), 25)
+        val alan = User(1L,"Alan","alan@gmail.com", LocalDate.of(2004, MAY, 10))
+        val alex = User(2L, "Alex","alex@gmail.com", LocalDate.of(1974, AUGUST, 10))
+        val anil = User(3L,"Anil","anil@gmail.com",LocalDate.of(2000, MAY, 10))
 
         userRepository.saveAll(listOf(alan, alex, anil))
     }
