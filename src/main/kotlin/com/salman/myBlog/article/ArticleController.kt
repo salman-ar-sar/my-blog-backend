6d203a568fb7d7fcb356e35775a6f6a0e68b62c8
Salman A <salman.a@hatio.in>
2021-09-27 13:26:45 +0530

    Add article controller

---
+++ src/main/kotlin/com/salman/myBlog/article/ArticleController.kt
@@ -0,0 +1,33 @@
+package com.salman.myBlog.article
+
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping(path = ["api/v1/articles"])
+class ArticleController(private val articleService: ArticleService) {
+
+    @GetMapping
+    fun getArticles(): List<Article> {
+        return articleService.getArticles()
+    }
+
+    @PostMapping
+    fun registerArticle(@RequestBody article: Article) {
+        articleService.addArticle(article)
+    }
+
+    @DeleteMapping(path = ["{articleId}"])
+    fun deleteArticle(@PathVariable("articleId") articleId: Long) {
+        articleService.deleteArticle(articleId)
+    }
+
+    @PutMapping(path = ["{articleId}"])
+    fun updateArticle(
+            @PathVariable("articleId") articleId: Long,
+            @RequestParam(required = false) name: String?,
+            @RequestParam(required = false) passwordHash: String?,
+            @RequestParam(required = false) profilePhoto: String?,
+            @RequestParam(required = false) email: String?) {
+        articleService.updateArticle(articleId, name, email, passwordHash, profilePhoto)
+    }
+}
